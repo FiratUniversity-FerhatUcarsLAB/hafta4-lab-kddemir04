13958a4cbe8cc508de965662590498ef550a13b8
kddemir04 <240541007@firat.edu.tr>
2025-10-29 18:28:48 +0300

    Update OgrenciBilgi.java

--- OgrenciBilgi.java
+++ OgrenciBilgi.java
@@ -1,7 +1,7 @@
 /*
- * Ad Soyad: [ADINIZI BURAYA YAZIN]
- * Ogrenci No: [OGRENCI NUMARANIZI BURAYA YAZIN]
- * Tarih: [TARIHI BURAYA YAZIN]
+ * Ad Soyad: [Abdulkadir Demir]
+ * Ogrenci No: [240541007]
+ * Tarih: [29/10/2025]
  * Aciklama: Gorev 1 - Ogrenci Bilgi Sistemi
  * 
  * Bu program kullanicidan ogrenci bilgilerini alir ve
@@ -12,49 +12,36 @@
 import java.util.Scanner;
 
 public class OgrenciBilgi {
-    public static void main(String[] args) {
-        // Scanner objesi olusturun
-        
-        
-        // Degisken tanimlamalari
-        // String ad, soyad;
-        // int ogrenciNo, yas;
-        // double gpa;
-        
-        
-        // Kullanicidan bilgileri alin
-        System.out.println("=== OGRENCI BILGI SISTEMI ===");
-        System.out.println();
-        
-        // Ad
-        
-        
-        // Soyad
-        
-        
-        // Ogrenci No
-        
-        
-        // Yas
-        
-        
-        // GPA
-        
-        
-        // Bilgileri ekrana yazdirin
-        System.out.println("\n=== OGRENCI BILGI SISTEMI ===");
-        
-        // COZUMUNUZU BURAYA YAZIN
-        
-        
-        
-        
-        
-        
 
-        
-        
-        // Scanner'i kapatin (önemli pratik)
-        
+    public static void main(String[] args) {
+
+        Scanner tara = new  Scanner(System.in);
+
+        System.out.println("===ÖĞRENCİ BİLGİ SİSTEMİ===\n");
+
+        //girdiler
+        System.out.println("Adınızı giriniz");//Adını alıyorum
+        String ad = tara.nextLine();
+
+        System.out.println("\nSoyadınızı giriniz");//Soyadını alıyorum
+        String soyad = tara.nextLine();
+
+        System.out.println("\nÖğrenci numaranızı giriniz");//Öğrenci numarasını alıyorum
+        int ogrenciNo = tara.nextInt();
+
+        System.out.println("\nYaşınızı giriniz");//Yaşını alıyorum
+        int yas =  tara.nextInt();
+
+        System.out.println("\nOrtalamanızı giriniz");//Ortalamasını alıyorum
+        double gpa =  tara.nextDouble();
+
+        //çıktılar
+        System.out.println("\n===ÖĞRENCİ BİLGİ SİSTEMİ===");
+        System.out.printf("Ad soyad: %s %s", ad, soyad);
+        System.out.printf("\nÖğrenci no: %d", ogrenciNo);//decimal
+        System.out.printf("\nYaş: %d", yas);
+        System.out.printf("\nOrtalama: %.2f", gpa);
+
+        tara.close();
     }
 }
